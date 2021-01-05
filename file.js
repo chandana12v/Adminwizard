dbdb5c64fd7b65fbd0cf9736b0b4661ac1dfc983
chandana12v <70066605+chandana12v@users.noreply.github.com>
2021-01-05 11:25:25 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,28 @@
+
+(function($){
+	function floatLabel(inputType){
+		$(inputType).each(function(){
+			var $this = $(this);
+			// on focus add cladd active to label
+			$this.focus(function(){
+				$this.next().addClass("active");
+			});
+			//on blur check field and remove class if needed
+			$this.blur(function(){
+				if($this.val() === '' || $this.val() === 'blank'){
+					$this.next().removeClass();
+				}
+			});
+		});
+	}
+	// just add a class of "floatLabel to the input field!"
+    floatLabel(".floatLabel");
+    
+    $('.submit').click(function(){
+        $('#orgName').val('');
+        $('#pName').val('');
+        $('#orgType').val('');
+        $('#OrgTier').val('');
+        $('.controls label.active').css({'top':'12px','color':'#999'})
+    })
+})(jQuery);
